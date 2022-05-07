567102cbc5d77f16a055e89604ce7c35ed530f28
Stijn Rogiest <reddusted@gmail.com>
2022-05-07 13:18:27 +0200

    Create WEBSITE_ROOT const

--- src/connection.js
+++ src/connection.js
@@ -1,3 +1,6 @@
+const WEBSITE_ROOT = "http://localhost:3000";
+const NO_ACTIVITY_TIME = 1000 * 60;
+
 let pubnub = new PubNub({
     publishKey: "pub-c-11a13158-acff-4307-85c9-aa2948d65e7c",
     subscribeKey: "sub-c-87a3e456-cbb2-11ec-ba93-a6fdca316470",
