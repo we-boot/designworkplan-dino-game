f72b806c45999c489dab1879935ba67a69f09dbe
Stijn Rogiest <reddusted@gmail.com>
2022-05-13 16:17:01 +0200

    Use subscribleRoom in DOMContentLoaded

--- connection.js
+++ connection.js
@@ -1,4 +1,3 @@
-// const WEBSITE_ROOT = "https://we-boot.github.io/designworkplan-dino-game";
 const WEBSITE_ROOT = location.hostname === "localhost" ? "http://localhost:3000" : "https://we-boot.github.io/designworkplan-dino-game";
 const NO_ACTIVITY_TIME = 1000 * 60;
 
