3ceb698b31e83b2099b61eb90c14a141f79f9acf
Stijn Rogiest <reddusted@gmail.com>
2022-05-13 16:12:23 +0200

    Generate QR-code for different room ids. Dynamic WEBSITE_ROOT constant

--- connection.js
+++ connection.js
@@ -1,4 +1,5 @@
-const WEBSITE_ROOT = "https://we-boot.github.io/designworkplan-dino-game";
+// const WEBSITE_ROOT = "https://we-boot.github.io/designworkplan-dino-game";
+const WEBSITE_ROOT = location.hostname === "localhost" ? "http://localhost:3000" : "https://we-boot.github.io/designworkplan-dino-game";
 const NO_ACTIVITY_TIME = 1000 * 60;
 
 let pubnub = new PubNub({
