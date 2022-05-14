c510767b486a8eac4f58793413cfd9d179d14b98
Stijn Rogiest <reddusted@gmail.com>
2022-05-14 16:43:02 +0200

    Fetch weather data from openweatherapi and show on screen

---
+++ weather.js
@@ -0,0 +1,16 @@
+const OPENWEATHER_API_KEY = "6659227eef739663c8ddbcb8fc1a472b";
+const OPENWEATHER_LAT = 52.3545828;
+const OPENWEATHER_LONG = 4.763878;
+
+async function getWeatherDescription() {
+    let res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${OPENWEATHER_LAT}&lon=${OPENWEATHER_LONG}&appid=${OPENWEATHER_API_KEY}`
+    );
+
+    if (res.ok) {
+        let data = await res.json();
+        return `${data.weather[0].description}\n${data.main.temp}°C`;
+    } else {
+        return "";
+    }
+}
