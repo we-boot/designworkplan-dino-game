186808ace402fd9cfc93ca9e350b1830af51ffa1
designworkplan <sander@designworkplan.com>
2022-05-31 09:02:17 +0200

    Update scenes.js

--- scenes.js
+++ scenes.js
@@ -14,7 +14,7 @@ window.addEventListener("keydown", (ev) => {
             url = "/screen/sign.html?title=Google%20L8%20Demo&rotation=0";
             break;
           case "5":
-            url = "/screen/sign.html?title=MicroKitchen%20Late%20Nigth&20Snacks&rotation=90";
+            url = "/screen/sign.html?title=MicroKitchen%20Late%20Night%20Snacks&rotation=180";
             break;
           case "0":
             url = "/screen/qr.html";
