3eccec58d970a0294f807961be16ec07fa2a8653
Stijn Rogiest <reddusted@gmail.com>
2022-06-30 19:39:51 +0200

    Create forms for media and sign pages

--- scenes.js
+++ scenes.js
@@ -8,16 +8,19 @@ window.addEventListener("keydown", (ev) => {
             url = "/screen/blocks";
             break;
         case "3":
-            url = "/screen/sign?title=Entrance&rotation=0";
+            url = "/screen/sign";
             break;
-        case "4":
-            url = "/screen/sign?title=Google%20L8%20Demo&rotation=0";
-            break;
-        case "5":
-            url = "/screen/sign?title=MicroKitchen%20Late%20Night%20Snacks&rotation=180";
-            break;
+        // case "3":
+        //     url = "/screen/sign?title=Entrance&rotation=0";
+        //     break;
+        // case "4":
+        //     url = "/screen/sign?title=Google%20L8%20Demo&rotation=0";
+        //     break;
+        // case "5":
+        //     url = "/screen/sign?title=MicroKitchen%20Late%20Night%20Snacks&rotation=180";
+        //     break;
         case "6":
-            url = "/screen/media?src=";
+            url = "/screen/media";
             break;
         case "7":
             url = "/screen/doodle";
